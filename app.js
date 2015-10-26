d14e54f149a0bcce8cd8b354c6bd34cca72d659b
Blake Hagen <blakehagen@gmail.com>
2015-10-25 20:49:44 -0600

    cash start value

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 $(document).ready(function(){
   
   
-  
-    
-    
+
+        
   // // // Card Constructor // // //
-  var id = 1;
-  var Card = function(card, suit, value, altValue){
+  var Card = function(card, suit, id, value, altValue){
     this.card = card;
     this.suit = suit;
+    this.id = id;
     this.value = value;
     this.altValue = altValue;
-    this.id = id++;
-    this.shuffle = function(){
+    this.whoAmI = function(){
       console.log("I'm a " + this.card + " of " + this.suit + ".");
     };
+    if(suit === "hearts"){
+      this.imgTag = '<img src="assets/heart.png">';
+      this.bottomImg = '<img src="assets/heart-inverted.png">'
+    } else if(suit === "spades"){
+       this.imgTag = '<img src="assets/spade.png">';
+       this.bottomImg = '<img src="assets/spade-inverted.png">';
+    } else if(suit === "clubs"){
+       this.imgTag = '<img src="assets/club.png">';
+       this.bottomImg = '<img src="assets/club-inverted.png">';
+    } else if(suit === "diamonds"){
+       this.imgTag = '<img src="assets/diamond.png">';
+       this.bottomImg = '<img src="assets/diamond-inverted.png">';
+    }
   };
   
   // // // // GET FRESH DECK OF CARDS // // // //
@@ -23,70 +34,70 @@ $(document).ready(function(){
   
     var deck = [];
     
-    var twoHearts = new Card("two", "hearts", 2);
-    var twoSpades = new Card("two", "spades", 2);
-    var twoClubs = new Card("two", "clubs", 2);
-    var twoDiamonds = new Card("two", "diamonds", 2);
+    var twoHearts = new Card("two", "hearts", "2",  2);
+    var twoSpades = new Card("two", "spades", "2", 2);
+    var twoClubs = new Card("two", "clubs", "2", 2);
+    var twoDiamonds = new Card("two", "diamonds", "2", 2);
     
-    var threeHearts = new Card("three", "hearts", 3);
-    var threeSpades = new Card("three", "spades", 3);
-    var threeClubs = new Card("three", "clubs", 3);
-    var threeDiamonds = new Card("three", "diamonds", 3);
+    var threeHearts = new Card("three", "hearts", "3", 3);
+    var threeSpades = new Card("three", "spades", "3", 3);
+    var threeClubs = new Card("three", "clubs", "3", 3);
+    var threeDiamonds = new Card("three", "diamonds", "3", 3);
     
-    var fourHearts = new Card("four", "hearts", 4);
-    var fourSpades = new Card("four", "spades", 4);
-    var fourClubs = new Card("four", "clubs", 4);
-    var fourDiamonds = new Card("four", "diamonds", 4);
+    var fourHearts = new Card("four", "hearts", "4", 4);
+    var fourSpades = new Card("four", "spades", "4", 4);
+    var fourClubs = new Card("four", "clubs", "4",  4);
+    var fourDiamonds = new Card("four", "diamonds", "4", 4);
     
-    var fiveHearts = new Card("five", "hearts", 5);
-    var fiveSpades = new Card("five", "spades", 5);
-    var fiveClubs = new Card("five", "clubs", 5);
-    var fiveDiamonds = new Card("five", "diamonds", 5);
+    var fiveHearts = new Card("five", "hearts", "5", 5);
+    var fiveSpades = new Card("five", "spades", "5", 5);
+    var fiveClubs = new Card("five", "clubs", "5", 5);
+    var fiveDiamonds = new Card("five", "diamonds", "5", 5);
     
-    var sixHearts = new Card("six", "hearts", 6);
-    var sixSpades = new Card("six", "spades", 6);
-    var sixClubs = new Card("six", "clubs", 6);
-    var sixDiamonds = new Card("six", "diamonds", 6);
+    var sixHearts = new Card("six", "hearts", "6", 6);
+    var sixSpades = new Card("six", "spades", "6", 6);
+    var sixClubs = new Card("six", "clubs", "6", 6);
+    var sixDiamonds = new Card("six", "diamonds", "6", 6);
     
-    var sevenHearts = new Card("seven", "hearts", 7);
-    var sevenSpades = new Card("seven", "spades", 7);
-    var sevenClubs = new Card("seven", "clubs", 7);
-    var sevenDiamonds = new Card("seven", "diamonds", 7);
+    var sevenHearts = new Card("seven", "hearts", "7", 7);
+    var sevenSpades = new Card("seven", "spades", "7", 7);
+    var sevenClubs = new Card("seven", "clubs", "7", 7);
+    var sevenDiamonds = new Card("seven", "diamonds", "7", 7);
     
-    var eightHearts = new Card("eight", "hearts", 8);
-    var eightSpades = new Card("eight", "spades", 8);
-    var eightClubs = new Card("eight", "clubs", 8);
-    var eightDiamonds = new Card("eight", "diamonds", 8);
+    var eightHearts = new Card("eight", "hearts", "8", 8);
+    var eightSpades = new Card("eight", "spades", "8", 8);
+    var eightClubs = new Card("eight", "clubs", "8", 8);
+    var eightDiamonds = new Card("eight", "diamonds", "8", 8);
     
-    var nineHearts = new Card("nine", "hearts", 9);
-    var nineSpades = new Card("nine", "spades", 9);
-    var nineClubs = new Card("nine", "clubs", 9);
-    var nineDiamonds = new Card("nine", "diamonds", 9);
+    var nineHearts = new Card("nine", "hearts", "9", 9);
+    var nineSpades = new Card("nine", "spades", "9", 9);
+    var nineClubs = new Card("nine", "clubs","9", 9);
+    var nineDiamonds = new Card("nine", "diamonds", "9", 9);
     
-    var tenHearts = new Card("ten", "hearts", 10);
-    var tenSpades = new Card("ten", "spades", 10);
-    var tenClubs = new Card("ten", "clubs", 10);
-    var tenDiamonds = new Card("ten", "diamonds", 10);
+    var tenHearts = new Card("ten", "hearts", "10", 10);
+    var tenSpades = new Card("ten", "spades", "10", 10);
+    var tenClubs = new Card("ten", "clubs", "10", 10);
+    var tenDiamonds = new Card("ten", "diamonds", "10", 10);
     
-    var jackHearts = new Card("jack", "hearts", 10);
-    var jackSpades = new Card("jack", "spades", 10);
-    var jackClubs = new Card("jack", "clubs", 10);
-    var jackDiamonds = new Card("jack", "diamonds", 10);
+    var jackHearts = new Card("jack", "hearts", "J", 10);
+    var jackSpades = new Card("jack", "spades", "J", 10);
+    var jackClubs = new Card("jack", "clubs", "J", 10);
+    var jackDiamonds = new Card("jack", "diamonds", "J", 10);
     
-    var queenHearts = new Card("queen", "hearts", 10);
-    var queenSpades = new Card("queen", "spades", 10);
-    var queenClubs = new Card("queen", "clubs", 10);
-    var queenDiamonds = new Card("queen", "diamonds", 10);
+    var queenHearts = new Card("queen", "hearts", "Q", 10);
+    var queenSpades = new Card("queen", "spades", "Q", 10);
+    var queenClubs = new Card("queen", "clubs", "Q", 10);
+    var queenDiamonds = new Card("queen", "diamonds", "Q", 10);
     
-    var kingHearts = new Card("king", "hearts", 10);
-    var kingSpades = new Card("king", "spades", 10);
-    var kingClubs = new Card("king", "clubs", 10);
-    var kingDiamonds = new Card("king", "diamonds", 10);
+    var kingHearts = new Card("king", "hearts","K", 10);
+    var kingSpades = new Card("king", "spades", "K", 10);
+    var kingClubs = new Card("king", "clubs", "J", 10);
+    var kingDiamonds = new Card("king", "diamonds", "K", 10);
     
-    var aceHearts = new Card("ace", "hearts", 11, 1);
-    var aceSpades = new Card("ace", "spades", 11, 1);
-    var aceClubs = new Card("ace", "clubs", 11, 1);
-    var aceDiamonds = new Card("ace", "diamonds", 11, 1);
+    var aceHearts = new Card("ace", "hearts", "A", 11, 1);
+    var aceSpades = new Card("ace", "spades", "A", 11, 1);
+    var aceClubs = new Card("ace", "clubs", "A", 11, 1);
+    var aceDiamonds = new Card("ace", "diamonds", "A", 11, 1);
     
     // Push Cards into 'deck' array //
     deck.push(twoHearts,twoSpades,twoClubs,twoDiamonds,threeHearts,threeSpades,threeClubs,threeDiamonds,fourHearts,fourSpades,fourClubs,fourDiamonds,fiveHearts,fiveSpades,fiveClubs,fiveDiamonds,sixHearts,sixSpades,sixClubs,sixDiamonds,sevenHearts,sevenSpades,sevenClubs,sevenDiamonds,eightHearts,eightSpades,eightClubs,eightDiamonds,nineHearts,nineSpades,nineClubs,nineDiamonds,tenHearts,tenSpades,tenClubs,tenDiamonds,jackHearts,jackSpades,jackClubs,jackDiamonds,queenHearts,queenSpades,queenClubs,queenDiamonds,kingHearts,kingSpades,kingClubs,kingDiamonds,aceHearts,aceSpades,aceClubs,aceDiamonds);
@@ -94,7 +105,7 @@ $(document).ready(function(){
     return deck;
   };
   
-  var deck = freshDeck(); // --> returns unshuffled fresh deck of cards
+  //var deck = freshDeck(); // --> returns unshuffled fresh deck of cards
   
   // // // // FUNCTION TO SHUFFLE THE DECK (("Fisher-Yates" Algorithm)) // // // //
   
@@ -114,7 +125,7 @@ $(document).ready(function(){
   
   // // // // SHUFFLE THE DECK // // // //
   
-  shuffle(deck); // --> Now you have a shuffled deck! //
+  //shuffle(deck); // --> Now you have a shuffled deck! //
   
   // // // // DEAL // // // //
   
@@ -129,7 +140,7 @@ $(document).ready(function(){
       arr.splice(0,4);
   };
   
-  deal(deck);
+  //deal(deck);
   
   // // // // SET ORIGINAL HAND VALUES // // // //
   
@@ -162,18 +173,40 @@ $(document).ready(function(){
     deck.splice(0,1);
   }
   
-  // // // // RESET HANDS BACK TO ZERO and RESET DECK // // // //
+  // // // // RESET HANDS BACK TO ZERO and RESET DECK and NEW DEAL // // // //
+  var cashCount = 500;
   
-  function reset(freshDeck){
+  function startNewGame(freshDeck){
+    cashCount = 500;
     playerHand = [];
     player = 0;
     dealerHand = [];
     dealer = 0;
     deck = freshDeck();
-    console.log("Reset complete");
+    shuffle(deck);
+    deal(deck);
   };
   
+  // On "Start New Game" button click, activate above function //
   
+  $('#startNewGame').on('click', function(){
+    startNewGame(freshDeck);
+    
+    $('#cashCount').empty();
+    $('#cashCount').append(cashCount);
+    
+    
+    
+    
+    $('#dealerCards').empty();
+    $('#playerCards').empty();
+ 
+    $('#dealerCards').append('<div class="card hidden"></div><div class="card"><div class="card-suit suit-left">' + dealerHand[1].imgTag + '</div><div class="card-text"><h2>' + dealerHand[1].id+ '</h2></div><div class="card-suit suit-right">' + dealerHand[1].bottomImg + '</div></div>');
+    
+    $('#playerCards').append('<div class="card"><div class="card-suit suit-left">' + playerHand[0].imgTag + '</div><div class="card-text"><h2>' + playerHand[0].id+ '</h2></div><div class="card-suit suit-right">' + playerHand[0].bottomImg + '</div></div><div class="card"><div class="card-suit suit-left">' + playerHand[1].imgTag + '</div><div class="card-text"><h2>' + playerHand[1].id+ '</h2></div><div class="card-suit suit-right">' + playerHand[1].bottomImg + '</div></div>');
+    
+    
+  });
     
     
     
