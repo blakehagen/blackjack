a698f5cff3771a481d80cfe0250ef633000ca46c
Blake Hagen <blakehagen@gmail.com>
2015-10-30 15:13:02 -0600

    headers responsive for reals

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   
     // HIDE NON_ESSENTIAL BUTTONS ON LOAD //
 
-    $('#nextHand').hide();
+    $('#nextHand').show();
     $('.hands-button').hide();
     $('#cash-view').hide();
     $('#stay-button').hide();
@@ -220,7 +220,7 @@ $(document).ready(function () {
   
     $('#startNewGame').on('click', function () {
         startNewGame(freshDeck);
-        $('#nextHand').hide();
+        $('#nextHand').show();
         $('.hands-button').show();
         $('#cash-view').show();
         $('#message-text').empty();
